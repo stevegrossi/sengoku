df3680f04ee04c2cdf2c6d92967f6140c973ff6e
Steve Grossi <me@stevegrossi.com>
2017-08-30 06:12:46 -0400

    Convert existing game code to React
    Ahh, that’s better.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,32 +1,18 @@
-// Brunch automatically concatenates all files in your
-// watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
-//
-// However, those files will only be executed if
-// explicitly imported. The only exception are files
-// in vendor, which are never wrapped in imports and
-// therefore are always executed.
-
-// Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-import socket from "./socket"
-
-// Boot up React
 import React from "react"
 import ReactDOM from "react-dom"
+import { Socket } from 'phoenix'
 import Game from "./components/Game"
 
 const game_id = window.game_id
 const game_container = document.getElementById('game_container')
 if (game_id && game_container) {
-  ReactDOM.render(<Game id={game_id} />, game_container)
+  const socket = new Socket('/socket', {params: {token: null}})
+  socket.connect()
+  const channel = socket.channel('games:' + game_id, {})
+  channel.join()
+    .receive('ok', resp => {
+      console.log('Joined successfully', resp)
+      ReactDOM.render(<Game id={game_id} channel={channel} />, game_container)
+    })
+    .receive('error', resp => { console.error('Unable to join game channel :(', resp) })
 }
--- assets/js/components/Game.js
+++ assets/js/components/Game.js
@@ -1,10 +1,38 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Socket } from 'phoenix'
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    const self = this;
+    this.props.channel.on('update', new_state => {
+      self.setState(new_state)
+    })
+  }
+
+  endTurn() {
+    this.props.channel.push('end_turn')
+  }
+
+  placeArmy() {
+    this.props.channel.push('place_armies', { count: 1, territory: 1 })
+  }
+
   render() {
     return (
-      <div>Hi from React! This is Game {this.props.id}</div>
+      <div>
+        <p>This is Game <b>{this.props.id}</b></p>
+        <button onClick={this.endTurn.bind(this)}>End Turn</button>
+        <button onClick={this.placeArmy.bind(this)}>Place Army</button>
+        <pre>
+          {JSON.stringify(this.state, null, 2)}
+        </pre>
+      </div>
     )
   }
 }

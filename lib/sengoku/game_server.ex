e226a83197a1308b5240f8b421350f7fec591b13
Steve Grossi <me@stevegrossi.com>
2017-08-31 10:56:33 -0400

    Assign territories programmatically
    Eventually this should be randomized

--- lib/sengoku/game_server.ex
+++ lib/sengoku/game_server.ex
@@ -23,7 +23,12 @@ defmodule Sengoku.GameServer do
       {:ok, _pid} -> {:ok, game_id}
       {:error, reason} -> {:error, reason}
     end
-    {:ok, initial_state(game_id)}
+    state =
+      game_id
+      |> get_initial_state()
+      |> assign_territories()
+
+    {:ok, state}
   end
 
   # API
@@ -84,22 +89,29 @@ defmodule Sengoku.GameServer do
     {:via, Registry, {:game_server_registry, game_id}}
   end
 
-  defp initial_state(game_id) do
+  defp assign_territories(state) do
+    territory_ids = Map.keys(state.territories)
+    Enum.reduce(Map.keys(@players), state, fn(player_id, state) ->
+      put_in(state, [:territories, player_id * 6, :owner], player_id)
+    end)
+  end
+
+  defp get_initial_state(game_id) do
     %{
       game_id: game_id,
       turn: 1,
       current_player_id: List.first(Map.keys(@players)),
       players: @players,
       territories: %{
-        1 => %{ owner: 1, armies: 0},
-        2 => %{ owner: nil, armies: 0},
-        3 => %{ owner: 2, armies: 0},
-        4 => %{ owner: nil, armies: 0},
-        5 => %{ owner: nil, armies: 0},
-        6 => %{ owner: nil, armies: 0},
-        7 => %{ owner: 3, armies: 0},
-        8 => %{ owner: nil, armies: 0},
-        9 => %{ owner: 4, armies: 0},
+         1 => %{ owner: nil, armies: 0},
+         2 => %{ owner: nil, armies: 0},
+         3 => %{ owner: nil, armies: 0},
+         4 => %{ owner: nil, armies: 0},
+         5 => %{ owner: nil, armies: 0},
+         6 => %{ owner: nil, armies: 0},
+         7 => %{ owner: nil, armies: 0},
+         8 => %{ owner: nil, armies: 0},
+         9 => %{ owner: nil, armies: 0},
         10 => %{ owner: nil, armies: 0},
         11 => %{ owner: nil, armies: 0},
         12 => %{ owner: nil, armies: 0},

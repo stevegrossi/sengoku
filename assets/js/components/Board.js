608f11e82f24085bb34d7846d49fba5617cf4814
Steve Grossi <me@stevegrossi.com>
2017-09-04 05:54:46 -0400

    Nicer territory selection visuals
    An inner glow rather than doubling the centered border

--- assets/js/components/Board.js
+++ assets/js/components/Board.js
@@ -13,13 +13,19 @@ const Board = (props) => {
     return (
       <g key={id}
          transform={tile.translate}
-         fill={playerUI[data.owner] && playerUI[data.owner].color || '#fff'}
+         fill={playerUI[data.owner] && playerUI[data.owner].color || '#d4c098'}
          stroke="#000"
          onClick={(e) => props.tileClicked(id, e)}
-         strokeWidth={is_selected ? '1.5' : '0.75'}
-         opacity={neighbor_of_selected ? '0.5' : '1'}>
+         strokeWidth="0.75">
 
         {tile.path}
+        <clipPath id={'clip-path-' + id}>{tile.path}</clipPath>
+        {is_selected &&
+          <g clipPath={'url(#clip-path-' + id + ')'} fill="transparent" strokeWidth="5" stroke="rgba(255,255,255,.5)">{tile.path}</g>
+        }
+        {neighbor_of_selected &&
+          <g clipPath={'url(#clip-path-' + id + ')'} fill="transparent" strokeWidth="5" stroke="rgba(255,255,255,.25)">{tile.path}</g>
+        }
 
         <text className="Tile-count"
               stroke="none"

33bbeba24700a5cb9b9a2ef5ca461d44a8bf7bce
Steve Grossi <me@stevegrossi.com>
2017-09-06 16:32:16 -0400

    Deploy time!
    Fantastic guide! https://hexdocs.pm/phoenix/heroku.html#content

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :sengoku, SengokuWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "sengoku-elixir.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,7 +60,3 @@ config :logger, level: :info
 #
 #     config :sengoku, SengokuWeb.Endpoint, server: true
 #
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"

79cb7f1540914cb9751223e37eb1671404ec3a6d
Steve Grossi <me@stevegrossi.com>
2017-09-07 18:19:57 -0400

    Nice border transition effects

--- assets/js/components/Board.js
+++ assets/js/components/Board.js
@@ -9,29 +9,26 @@ const Board = (props) => {
     const tile = boardData[id]
     const is_selected = props.selectedTileId == id
     const neighbor_of_selected = neighbors && neighbors.indexOf(id) > -1
+    let borderClassNames = ['Tile-border']
+    is_selected && borderClassNames.push('Tile-border--major')
+    neighbor_of_selected && borderClassNames.push('Tile-border--minor')
 
     return (
       <g key={id}
+         className="Tile"
          transform={tile.translate}
          fill={playerUI[data.owner] && playerUI[data.owner].color || '#d4c098'}
-         stroke="#000"
-         onClick={(e) => props.tileClicked(id, e)}
-         strokeWidth="0.75">
+         onClick={(e) => props.tileClicked(id, e)}>
 
-        {tile.path}
+        <g className="Tile-background">{tile.path}</g>
         <clipPath id={'clip-path-' + id}>{tile.path}</clipPath>
-        {is_selected &&
-          <g clipPath={'url(#clip-path-' + id + ')'} fill="transparent" strokeWidth="5" stroke="rgba(255,255,255,.5)">{tile.path}</g>
-        }
-        {neighbor_of_selected &&
-          <g clipPath={'url(#clip-path-' + id + ')'} fill="transparent" strokeWidth="5" stroke="rgba(255,255,255,.25)">{tile.path}</g>
-        }
-
+        <g className={borderClassNames.join(' ')} clipPath={'url(#clip-path-' + id + ')'} fill="transparent" strokeWidth="5" stroke="rgba(255,255,255,.5)">{tile.path}</g>
         <text className="Tile-count"
               stroke="none"
               x={tile.tx}
               y={tile.ty}
               textAnchor="middle">{data.units}</text>
+        <g className="Tile-highlight">{tile.path}</g>
       </g>
     )
   })
@@ -43,8 +40,7 @@ const Board = (props) => {
          strokeWidth="1"
          fill="none"
          fillRule="evenodd"
-         children={tiles}>
-      </g>
+         children={tiles} />
     </svg>
   )
 }

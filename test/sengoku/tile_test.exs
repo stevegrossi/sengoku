9b9e9e67c82fc31c94b0524423156567ed34d1ed
SherSpock <wrtimberlake@gmail.com>
2017-10-26 13:58:33 -0400

    Added tests and functionality for Tile.owned_by_player_id?

---
+++ test/sengoku/tile_test.exs
@@ -0,0 +1,25 @@
+defmodule Sengoku.TileTest do
+    use ExUnit.Case, async: true
+   
+    alias Sengoku.{Game, Player, Tile}
+
+    describe "owned_by_player_id?/2" do
+        test "should return true if the passed tile is owned by the passed player" do
+          tile = %Tile{owner: 1}
+          player_id = 1
+          assert Tile.owned_by_player_id?(tile, player_id)
+        end    
+
+        test "should return false if the passed tile is owned by another player" do
+          tile = %Tile{owner: 2}
+          player_id = 1
+          refute Tile.owned_by_player_id?(tile, player_id)  
+        end
+
+        test "should return false if the passed tile is not owned by anyone" do
+          tile = %Tile{}
+          player_id = 1
+          refute Tile.owned_by_player_id?(tile, player_id)  
+        end    
+    end    
+end

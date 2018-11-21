9f21eaa0587abb8527b5d5238b05226748af0d59
Steve Grossi <me@stevegrossi.com>
2018-11-21 10:34:23 -0500

    Brunch -> Webpack

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ use Mix.Config
 #
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
+# with webpack to recompile .js and .css sources.
 config :sengoku, SengokuWeb.Endpoint,
   http: [port: 4000],
   debug_errors: true,
@@ -13,9 +13,10 @@ config :sengoku, SengokuWeb.Endpoint,
   check_origin: false,
   watchers: [
     node: [
-      "node_modules/brunch/bin/brunch",
-      "watch",
-      "--stdin",
+      "node_modules/webpack/bin/webpack.js",
+      "--mode",
+      "development",
+      "--watch-stdin",
       cd: Path.expand("../assets", __DIR__)
     ]
   ]

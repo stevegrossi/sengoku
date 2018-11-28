a56f45fe387312f323ece0a7bd62ad6ac7b97f85
Steve Grossi <me@stevegrossi.com>
2018-11-28 06:20:57 -0500

    Bump dependencies
    And fix version mismatch in mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sengoku.Mixfile do
     [
       app: :sengoku,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,

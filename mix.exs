ca6574f577c20580d65c0ca8364e620fd7151254
Steve Grossi <me@stevegrossi.com>
2019-09-22 10:02:43 -0400

    Use build_embedded in prod
    As recommended by https://github.com/HashNuke/heroku-buildpack-elixir#other-notes

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Sengoku.Mixfile do
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

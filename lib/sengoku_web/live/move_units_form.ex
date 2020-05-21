bd556e0d7571efc5ea136db93c520ea61506a897
Steve Grossi <me@stevegrossi.com>
2020-05-21 14:07:24 -0400

    MoveUnitsForm: options for min, max, and default half

--- lib/sengoku_web/live/move_units_form.ex
+++ lib/sengoku_web/live/move_units_form.ex
@@ -1,27 +1,57 @@
 defmodule SengokuWeb.MoveUnitsForm do
   use Phoenix.LiveComponent
 
+  def update(assigns, socket) do
+    default_count = floor(assigns.required_move.max / 2)
+    {:ok, assign(socket, Map.merge(assigns, %{count: default_count}))}
+  end
+
   def render(assigns) do
     ~L"""
     <div class="Modal">
-      <form class="MoveForm" phx-submit="move">
+      <div class="MoveForm">
         <h2>Move how many?</h2>
-        <div class="MoveForm-slider">
+        <form
+          class="MoveForm-slider"
+          phx-change="update_count"
+          phx-target="<%= @myself %>"
+        >
           <span><%= @required_move.min %></span>
           <input class="MoveForm-input"
                  type="range"
                  min=<%= @required_move.min %>
                  max=<%= @required_move.max %>
                  name="count"
+                 value="<%= @count %>"
                  autoFocus
           />
           <span><%= @required_move.max %></span>
-        </div>
+        </form>
         <div class="MoveForm-actions">
-          <input class="Button Button--primary" type="submit" value="Move" />
+          <button
+            class="Button Button--primary"
+            phx-click="move"
+            phx-value-count="<%= @required_move.max %>"
+          >Move Max (<%= @required_move.max %>)</button>
+          <button
+            class="Button Button--primary"
+            phx-click="move"
+            phx-value-count="<%= @count %>"
+          >Move Selected (<%= @count %>)</button>
+          <button
+            class="Button Button--primary"
+            phx-click="move"
+            phx-value-count="<%= @required_move.min %>"
+          >Move Min (<%= @required_move.min %>)</button>
         </div>
-      </form>
+      </div>
     </div>
     """
+  end
+
+  @impl true
+  def handle_event("update_count", %{"count" => count_string}, socket) do
+    {count, _} = Integer.parse(count_string)
+    {:noreply, assign(socket, count: count)}
   end
 end
